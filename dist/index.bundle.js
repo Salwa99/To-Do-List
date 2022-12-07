0f2bc4952f456b8c98a6dafda1bc95dea3cc807d
Salwa99 <s.ritage99@gmail.com>
2022-12-07 13:30:38 +0100

    Fix Linters errors

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
